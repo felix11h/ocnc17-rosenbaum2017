9105c43926a6ed3efc8ebe85555d2f5587f64fbc
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2017-07-05 16:03:41 +0900

    raster and membrane voltage plots

--- blncd_net/img/plot_all.py
+++ blncd_net/img/plot_all.py
@@ -4,6 +4,7 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as pl
 
+import numpy as np
 from brian2.units import *
 
 import pickle
@@ -30,65 +31,80 @@ pl.rcParams['text.latex.preamble'] = [
 ]  
 
 
-# fig,ax = pl.subplots(2,1)
-# fig.set_size_inches(5,4.)
+fig,ax = pl.subplots(2,1)
+fig.set_size_inches(5,5.5)
 
-# Ts = 19500*ms
-# k = 3
+Ts = 19500*ms
+k = 1
 
-# ax[0].set_xlabel('t [s]')
-# ax[0].set_ylabel('V [mV]')
-# ax[0].plot(vi['t'][vi['t']>Ts],
-#         vi['V'].T[k][vi['t']>Ts]/mV)
-# ax[0].margins(0.01)
-
-
-# ax[1].set_xlabel('t [s]')
-# ax[1].set_ylabel('V [mV]')
-# ax[1].plot(vi['t'][vi['t']>Ts],
-#            vi['Ie_syn'].T[k][vi['t']>Ts]/mV,
-#            label=r'$I^{\text{exc}}_{\text{syn}}$')
-# ax[1].plot(vi['t'][vi['t']>Ts],
-#            vi['Ii_syn'].T[k][vi['t']>Ts]/mV,
-#            label=r'$I^{\text{inh}}_{\text{syn}}$')
-# ax[1].plot(vi['t'][vi['t']>Ts],
-#            (vi['Ie_syn'].T[k][vi['t']>Ts] + \
-#             vi['Ii_syn'].T[k][vi['t']>Ts])/mV,
-#            color='gray',
-#            label=r'$I^{\text{inh}}_{\text{syn}}' +\
-#                  r' + I^{\text{inh}}_{\text{syn}}$')
-
-# ax[1].set_ylim(-60,60)
-# ax[1].margins(0.01)
-
-# ax[1].legend(bbox_to_anchor=(0., 1.01, 1., .101),
-#              loc='lower center', framealpha=1., ncol=3,
-#              frameon=False)
+ax[0].set_xlabel('t [s]')
+ax[0].set_ylabel('V [mV]')
+ax[0].plot(Erec['t'][Erec['t']>Ts],
+        Erec['V'].T[k][Erec['t']>Ts]/mV)
+ax[0].margins(0.01)
 
 
+ax[1].set_xlabel('t [s]')
+ax[1].set_ylabel('V [mV]')
+ax[1].plot(Erec['t'][Erec['t']>Ts],
+           Erec['Ie_syn'].T[k][Erec['t']>Ts]/mV,
+           label=r'$I^{\text{exc}}_{\text{syn}}$')
+ax[1].plot(Erec['t'][Erec['t']>Ts],
+           Erec['Ii_syn'].T[k][Erec['t']>Ts]/mV,
+           label=r'$I^{\text{inh}}_{\text{syn}}$')
+ax[1].plot(Erec['t'][Erec['t']>Ts],
+           (Erec['Ie_syn'].T[k][Erec['t']>Ts] + \
+            Erec['Ii_syn'].T[k][Erec['t']>Ts])/mV,
+           color='gray',
+           label=r'$I^{\text{inh}}_{\text{syn}}' +\
+                 r' + I^{\text{inh}}_{\text{syn}}$')
 
-# pl.tight_layout(h_pad=3.25)
-# fig.savefig('name.png', dpi=300) #bbox_inches='tight')
+yl=np.max(np.abs(pl.ylim()))
+ax[1].set_ylim((-yl,yl))
+ax[1].margins(0.01)
+
+ax[1].legend(bbox_to_anchor=(0., 1.01, 1., .101),
+             loc='lower center', framealpha=1., ncol=3,
+             frameon=False)
 
 
 
+pl.tight_layout(h_pad=3.25)
+fig.savefig('name.png', dpi=300) #bbox_inches='tight')
 
-fig, ax = pl.subplots()
-fig.set_size_inches(6,3)
+
+
+
+fig, ax = pl.subplots(2,1)
+fig.set_size_inches(6,5.5)
 
 iN = 400
-tmin = 18000*ms
+tmin = 19000*ms
+ke = 0
+ki = 0
 
-ax.plot(espk['t'][(espk['t']>tmin) & (espk['i']<iN)],
+ax[0].plot((espk['t'][(espk['t']>tmin) & (espk['i']<iN)]-tmin)/ms,
         espk['i'][(espk['t']>tmin) & (espk['i']<iN)],
         '.', color='blue', markersize=.5)
-ax.plot(ispk['t'][(ispk['t']>tmin) & (ispk['i']<iN)],
+ax[0].plot((ispk['t'][(ispk['t']>tmin) & (ispk['i']<iN)]-tmin)/ms,
         iN+ispk['i'][(ispk['t']>tmin) & (ispk['i']<iN)],
         '.', color='red', markersize=.5)
 
-ax.margins(0.0)
-ax.set_xlabel('t [s]')
-ax.set_ylabel('index i')
+ax[0].margins(0.0)
+ax[0].set_xlabel('t [ms]')
+ax[0].set_ylabel('index i')
+
+tmin = 19900*ms
+
+ax[1].plot((Erec['t'][Erec['t']>tmin]-tmin)/ms,
+           Erec['Iex'][Erec['t']>tmin]/mV,
+           color='blue')
+ax[1].plot((Irec['t'][Irec['t']>tmin]-tmin)/ms,
+           Irec['Iex'][Irec['t']>tmin]/mV,
+           color='red')
+
+ax[1].set_xlabel('t [ms]')
+ax[1].set_ylabel('external input [mV]')
 
 pl.tight_layout()
 fig.savefig('new.png', dpi=300)
