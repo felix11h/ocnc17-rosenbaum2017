069fdd26765a67c69508bd5fe07e33a77a344f5c
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2017-07-06 18:04:33 +0900

    ffwd-rec fully implemented

--- ffwd_rec/ffwd_rec.py
+++ ffwd_rec/ffwd_rec.py
@@ -25,32 +25,39 @@ DelT_i = 0.5*mV
 ref_e = 1.5*ms
 ref_i = 0.5*ms
 
+
+tau_syn_e = 8*ms 
+tau_syn_i = 4*ms
+tau_syn_f = tau_syn_e
+
+j_ee = 40*mV / (N**0.5)
+j_ie = 120*mV / (N**0.5)
+j_ei = -400*mV / (N**0.5)
+j_ii = -400*mV / (N**0.5)
+j_eF = 120*mV / (N**0.5)
+j_iF = 120*mV / (N**0.5)
+
+a_rec  = 0.25
+re_nrows, re_ncols = 63,63
+ri_nrows, ri_ncols = 32,32
+assert((re_nrows**2==Ne) & (ri_nrows**2==Ni))
 Kee = 200
 Kei = 200
 Kie = 500
 Kii = 500
 
-tau_syn_e = 8*ms 
-tau_syn_i = 4*ms
-j_ee = 12.5*mV / (N**0.5)
-j_ie = 20*mV / (N**0.5)
-j_ei = -50*mV / (N**0.5)
-j_ii = -50*mV / (N**0.5)
+a_ffwd = 0.1
+f_nrows, f_ncols = 24, 24
 
-re_nrows, re_ncols = 63,63
-ri_nrows, ri_ncols = 32,32
-assert((re_nrows**2==Ne) & (ri_nrows**2==Ni))
-
-a_rec  = 0.25
-f_rows, f_cols = 24, 24
-a_ffwd = None
+KeF = 1000
+KiF = 80
 
 Nf = 529
 rf = 5*Hz
 
 method = 'rk2'
-T = 10*ms
-#T = 20000*ms
+#T = 10*ms
+T = 10000*ms
 
 
 model='''
@@ -62,10 +69,11 @@ tau  : second (constant)
 m    : volt   (constant)
 DelT : volt   (constant)
 
-dV/dt = 1/tau*(E_L-V) + 1/tau*DelT*exp((V-V_T)/DelT) + 1/tau*Ie_syn + 1/tau*Ii_syn: volt (unless refractory)
+dV/dt = 1/tau*(E_L-V) + 1/tau*DelT*exp((V-V_T)/DelT) + 1/tau*Ie_syn + 1/tau*Ii_syn +1/tau*If_syn: volt (unless refractory)
 
 dIe_syn/dt = -1/tau_syn_e * Ie_syn : volt
 dIi_syn/dt = -1/tau_syn_i * Ii_syn : volt
+dIf_syn/dt = -1/tau_syn_f * If_syn : volt
 
 ref : second (constant)
 '''
@@ -76,9 +84,6 @@ Ffwd = PoissonGroup(Nf, rf)
 NGrp = NeuronGroup(N, model, method=method,
                     threshold='V > V_th', reset='V = V_re',
                     refractory='ref')
-
-
-
 
 NErcr = NGrp[:Ne]
 NIrcr = NGrp[Ne:]
@@ -97,7 +102,6 @@ NErcr.DelT = DelT_e
 NIrcr.DelT = DelT_i
 
 
-
 def get_rcr_targets(Nsrc, src_nrows, Ntar, tar_nrows, K):
     tar_x = (np.repeat((np.arange(Nsrc) % src_nrows)/src_nrows, K)\
              + np.random.normal(0, a_rec, size=Nsrc*K)) % 1
@@ -106,7 +110,6 @@ def get_rcr_targets(Nsrc, src_nrows, Ntar, tar_nrows, K):
     ids = (tar_nrows-1)*np.rint(tar_nrows*tar_x).astype(int) \
           + np.rint((tar_nrows-1)*tar_y).astype(int)
     return ids
-
 
 S_ee = Synapses(NErcr, NErcr, on_pre='Ie_syn_post += j_ee')
 S_ie = Synapses(NErcr, NIrcr, on_pre='Ie_syn_post += j_ie')
@@ -123,16 +126,43 @@ S_ii.connect(i = np.repeat(np.arange(Ni),Kii),
              j = get_rcr_targets(Ni, ri_nrows, Ni, ri_nrows, Kii))
 
 
+def get_ffwd_targets(Ntar, tar_nrows, K):
+    tar_x = (np.repeat((np.arange(Nf) % f_nrows)/f_nrows, K)\
+             + np.random.normal(0, a_ffwd, size=Nf*K)) % 1
+    tar_y = (np.repeat((np.arange(Nf) / f_nrows)/f_nrows, K)\
+             + np.random.normal(0, a_ffwd, size=Nf*K)) % 1
+    ids = (tar_nrows-1)*np.rint(tar_nrows*tar_x).astype(int) \
+          + np.rint((tar_nrows-1)*tar_y).astype(int)
+    return ids
+
+S_eF = Synapses(Ffwd, NErcr, on_pre='If_syn_post += j_eF')
+S_iF = Synapses(Ffwd, NIrcr, on_pre='If_syn_post += j_iF')
+
+S_eF.connect(i = np.repeat(np.arange(Nf),KeF),
+             j = get_ffwd_targets(Ne, re_nrows, KeF))
+S_iF.connect(i = np.repeat(np.arange(Nf),KiF),
+             j = get_ffwd_targets(Ni, ri_nrows, KiF))
 
 
-Rrec  = StateMonitor(NGrp, ['V', 'Ie_syn', 'Ii_syn'],
+Rrec  = StateMonitor(NGrp, ['V', 'Ie_syn', 'Ii_syn', 'If_syn'],
                       record=[0,Ne])
 ESPKrec = SpikeMonitor(NErcr)
 ISPKrec = SpikeMonitor(NIrcr)
+FSPKrec = SpikeMonitor(Ffwd)
 
 NErcr.V = V_re
 NIrcr.V = V_re
-
 run(T, report='text')
 
+
+import os, pickle
+pyname = os.path.splitext(os.path.basename(__file__))[0]
+
+fname = "{:s}_N{:d}_T{:d}ms".format(pyname, N, int(T/ms)) 
+
+with open("data/"+fname+".p", "wb") as pfile:
+    pickle.dump(Rrec.get_states(),pfile)
+    pickle.dump(ESPKrec.get_states(), pfile)
+    pickle.dump(ISPKrec.get_states(), pfile)
+    pickle.dump(FSPKrec.get_states(), pfile)
 
