bb6c789c4548d85ac34cde759711b9cf8ccbfd74
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2017-07-06 20:02:33 +0900

    allow for different param sets

--- ffwd_rec/ffwd_rec.py
+++ ffwd_rec/ffwd_rec.py
@@ -1,4 +1,3 @@
-# http://brian2.readthedocs.io/en/2.0rc/examples/synapses.spatial_connections.html?highlight=spatial
 
 import matplotlib
 matplotlib.use('Agg')
@@ -6,59 +5,17 @@ import matplotlib.pyplot as pl
 
 from brian2 import *
 
+import sys, importlib
+params = importlib.import_module(sys.argv[1])
+
+params_dict = params.__dict__
+try:
+    to_import = params.__all__
+except AttributeError:
+    to_import = [name for name in params_dict if not name.startswith('_')]
+globals().update({name: params_dict[name] for name in to_import})
+
 set_device('cpp_standalone')
-
-Ne= 63**2
-Ni= 32**2
-N = Ne+Ni
-
-tau_e = 15*ms
-tau_i = 10*ms
-E_L  = -60*mV
-V_T  = -50*mV
-V_re = -65*mV
-V_th = -10*mV
-
-DelT_e = 2*mV 
-DelT_i = 0.5*mV
-
-ref_e = 1.5*ms
-ref_i = 0.5*ms
-
-
-tau_syn_e = 8*ms 
-tau_syn_i = 4*ms
-tau_syn_f = tau_syn_e
-
-j_ee = 40*mV / (N**0.5)
-j_ie = 120*mV / (N**0.5)
-j_ei = -400*mV / (N**0.5)
-j_ii = -400*mV / (N**0.5)
-j_eF = 120*mV / (N**0.5)
-j_iF = 120*mV / (N**0.5)
-
-a_rec  = 0.25
-re_nrows, re_ncols = 63,63
-ri_nrows, ri_ncols = 32,32
-assert((re_nrows**2==Ne) & (ri_nrows**2==Ni))
-Kee = 200
-Kei = 200
-Kie = 500
-Kii = 500
-
-a_ffwd = 0.1
-f_nrows, f_ncols = 24, 24
-
-KeF = 1000
-KiF = 80
-
-Nf = 529
-rf = 5*Hz
-
-method = 'rk2'
-#T = 10*ms
-T = 10000*ms
-
 
 model='''
 grd_id  : integer (constant)
