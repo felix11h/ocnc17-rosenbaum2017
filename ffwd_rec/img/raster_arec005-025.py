a3c501366aed7e1db76e425b9aceeac98a770c45
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2017-07-09 11:39:37 +0900

    mv plot_raster

--- ffwd_rec/img/raster_arec005-025.py
+++ ffwd_rec/img/raster_arec005-025.py
@@ -10,15 +10,15 @@ from utils import get_spks
 
 import pickle,sys
 
-large=False
+
 if sys.argv[1]=='large':
     fname005 = "rb_arec0.05_N50000_T10000ms"
     fname025 = "rb_arec0.25_N50000_T10000ms"
-    large=True
+    mode = 'large'
 else:
     fname005 = "red_arec0.05_N4993_T10000ms"
     fname025 = "red_arec0.25_N4993_T10000ms"
-
+    mode = 'small'
     
 matplotlib.rc('text', usetex=True)
 pl.rcParams['text.latex.preamble'] = [
@@ -29,22 +29,25 @@ pl.rcParams['text.latex.preamble'] = [
     r'\sisetup{detect-all}',    
 ]  
 
+with open('../data/'+fname025+'.p', 'rb') as pfile:
+    state025 = pickle.load(pfile)
 
+with open('../data/'+fname005+'.p', 'rb') as pfile:
+    state005 = pickle.load(pfile)
+
+
+# ------- random raster --------- #
+    
 fig, ax = pl.subplots(2,1)
 fig.set_size_inches(6,5.5)
-
-
-
-with open('../data/'+fname025+'.p', 'rb') as pfile:
-    state = pickle.load(pfile)
-    Erec = state['Erec']
-    Irec = state['Irec']
-    espk = state['ESPK']
-    ispk = state['ISPK']
 
 size = 400 
 tmin = 9000*ms
 tmax = 10000*ms
+
+state = state025
+espk = state['ESPK']
+ispk = state['ISPK']
 
 Ne, Ni = len(state['NErcr']['x']), len(state['NIrcr']['x'])
 Ne_slct = np.random.choice(np.arange(Ne), size, replace=False)
@@ -64,12 +67,10 @@ ax[0].set_xlabel('t [ms]')
 ax[0].set_ylabel('index i')
 ax[0].set_title(r'$\alpha_{\mathrm{rec}} = 0.25$')
 
-with open('../data/'+fname005+'.p', 'rb') as pfile:
-    state = pickle.load(pfile)
-    Erec = state['Erec']
-    Irec = state['Irec']
-    espk = state['ESPK']
-    ispk = state['ISPK']
+
+state = state005
+espk = state['ESPK']
+ispk = state['ISPK']
 
 size = 400 
 tmin = 9000*ms
@@ -95,7 +96,66 @@ ax[1].set_ylabel('index i')
 ax[1].set_title(r'$\alpha_{\mathrm{rec}} = 0.05$')
 
 pl.tight_layout()
-fig.savefig('rnd_raster.png', dpi=300)
+fig.savefig('raster_rnd_{:s}.png'.format(mode), dpi=300)
 
 
 
+# ------- first N --------- #
+    
+fig, ax = pl.subplots(2,1)
+fig.set_size_inches(6,5.5)
+
+size = 400 
+tmin = 9000*ms
+tmax = 10000*ms
+
+state = state025
+espk = state['ESPK']
+ispk = state['ISPK']
+
+Ne_slct = np.arange(size)
+Ni_slct = np.arange(size)
+
+ax[0].plot(*get_spks(espk, tmin, tmax, Ne_slct), 
+           marker='.', color='blue', markersize=.5,
+           linestyle='None')
+
+ispk_t, ispk_id = get_spks(ispk, tmin, tmax, Ni_slct) 
+ax[0].plot(ispk_t, ispk_id + size, 
+           marker='.', color='red', markersize=.5,
+           linestyle='None')
+
+ax[0].margins(0.0)
+ax[0].set_xlabel('t [ms]')
+ax[0].set_ylabel('index i')
+ax[0].set_title(r'$\alpha_{\mathrm{rec}} = 0.25$')
+
+
+state = state005
+espk = state['ESPK']
+ispk = state['ISPK']
+
+size = 400 
+tmin = 9000*ms
+tmax = 10000*ms
+
+Ne_slct = np.arange(size)
+Ni_slct = np.arange(size)
+
+ax[1].plot(*get_spks(espk, tmin, tmax, Ne_slct), 
+           marker='.', color='blue', markersize=.5,
+           linestyle='None')
+
+ispk_t, ispk_id = get_spks(ispk, tmin, tmax, Ni_slct) 
+ax[1].plot(ispk_t, ispk_id + size, 
+           marker='.', color='red', markersize=.5,
+           linestyle='None')
+
+
+ax[1].margins(0.0)
+ax[1].set_xlabel('t [ms]')
+ax[1].set_ylabel('index i')
+ax[1].set_title(r'$\alpha_{\mathrm{rec}} = 0.05$')
+
+pl.tight_layout()
+fig.savefig('raster_firstN_{:s}.png'.format(mode), dpi=300)
